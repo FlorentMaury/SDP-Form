d21559554c6024a1d396af9fe63f6ae9977198fa
Florent Maury <flo.neo@hotmail.fr>
2024-02-27 10:49:25 +0100

    COrrectifs de sécurité d'accès.

--- index.php
+++ index.php
@@ -10,53 +10,51 @@ require('./model/connectionDBModel.php');
 require('./controller/controller.php');
 
 // Pages valides.
-$validPages = ['home'];
+$validPages = ['home', 'search', 'logIn', 'logOut'];
 
 // Direction de l'utilisateur en fonction de la requête.
 try {
-    if (isset($_GET['page']) && isset($_SESSION['can_access_db'])) {
+    if (isset($_GET['page']) && in_array($_GET['page'], $validPages)) {
+        if (isset($_SESSION['can_access_db'])) {
+            // Page d'accueil.
+            if ($_GET['page'] == 'home') {
+                home();
+            }
+            // tableau de bord.
+            else if ($_GET['page'] == 'search') {
+                search();
+            }
+            // Page de connexion.
+            else if ($_GET['page'] == 'logIn') {
+                logIn();
+            }
+            // Deconnexion.
+            else if ($_GET['page'] == 'logOut') {
+                logOut();
+            }
+        } else if ($_GET['page'] == 'home' && isset($_GET['token'])) {
+            $token = $_GET['token'];
 
-        // Page d'accueil.
-        if ($_GET['page'] == 'home') {
-            home();
-        }
-        // tableau de bord.
-        else if ($_GET['page'] == 'search') {
-            search();
-        }
-        // Page de connexion.
-        else if ($_GET['page'] == 'logIn') {
+            // Préparez une requête SQL pour vérifier si le token existe déjà
+            $stmt = $bdd->prepare('SELECT 1 FROM customer WHERE token = ?');
+            $stmt->execute([$token]);
+            $row = $stmt->fetch();
+
+            if ($row) {
+                // Si le token existe déjà, appelez la fonction home() avec un message d'erreur
+                header('location: ./index.php?page=home&error=1&message=Vous êtes déjà enregistré.');
+                exit();
+            } else {
+                home();
+            }
+        } else {
+            // Retour accueil.
             logIn();
         }
-        // Deconnexion.
-        else if ($_GET['page'] == 'logOut') {
-            logOut();
-        }
-        // En cas de demande de page inconnue.
-        else {
-            throw new Exception("Cette page n'existe pas ou a été supprimée.");
-        }
-
-    } else if (isset($_GET['page'])&& in_array($_GET['page'], $validPages) && isset($_GET['token'])) {
-        $token = $_GET['token'];
-
-        // Préparez une requête SQL pour vérifier si le token existe déjà
-        $stmt = $bdd->prepare('SELECT 1 FROM customer WHERE token = ?');
-        $stmt->execute([$token]);
-        $row = $stmt->fetch();
-
-        if ($row) {
-            // Si le token existe déjà, appelez la fonction home() avec un message d'erreur
-            header('location: ./index.php?page=home&error=1&message=Vous êtes déjà enregistré.');
-        } else {
-            home();
-        }
     } else {
-        // Retour accueil.
-        logIn();
-    };
+        throw new Exception("Cette page n'existe pas ou a été supprimée.");
+    }
 }
-
 // En cas d'erreur.
 catch (Exception $e) {
     $error = $e->getMessage();
